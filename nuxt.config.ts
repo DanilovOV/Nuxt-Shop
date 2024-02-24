a62b1fe9fb1a1023bbdfe07f27e5ff454a05bd72
Олег Данилов <oleg-danilov8@ya.ru>
2024-02-25 00:35:14 +0300

    update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
+	pages: true,
 	modules: [
-		'@nuxtjs/tailwindcss',
-		'@pinia/nuxt',
-		// '@nuxtjs/supabase',
 		'nuxt-icon',
 		'nuxt-lodash',
+		'@pinia/nuxt',
+		'@pinia-plugin-persistedstate/nuxt',
+		'@nuxtjs/tailwindcss',
+		'@nuxtjs/supabase',
 	],
-	runtimeConfig: {
-		public: {
-			stripePk: process.env.STRIPE_PK_KEY,
-		},
-	},
-	app: {
-		head: {
-			script: [{ src: 'https://js.stripe.com/v3/', defer: true }],
-		},
-	},
 })
